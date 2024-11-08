7168c2f7e63e9e90a4862675045fbeadb2dba04f
Nikita Aniskov <nikitanikitaorenburg@gmail.com>
2024-11-08 19:03:45 +0300

    save

---
+++ 2пк2_Аниськов/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _2пк2_Аниськов
+{
+    internal class Program
+    {
+        static int secretNumber;
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            secretNumber = random.Next(0, 11); // загадываем число от 0 до 10
+            Console.WriteLine("Игра: Угадай число! У вас есть 3 попытки.");
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Введите ваше предположение: ");
+                int userGuess = int.Parse(Console.ReadLine());
+                TryToGuess(userGuess);
+                if (TryToGuess(userGuess))
+                {
+                    break;
+                }
+            }
+        }
+        static bool TryToGuess(int x)
+        {
+            if (x == secretNumber)
+            {
+                Console.WriteLine("Поздравляем! Вы угадали число.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Неверно. Попробуйте ещё раз.");
+                return false;
+            }
+        }
+    }
+}
